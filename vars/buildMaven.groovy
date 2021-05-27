49724fcac312714d429bb98c48b419fe1f4dbe26
Your Name <vitaliy.khatrus@gmail.com>
2021-05-27 16:50:26 +0300

    test service_name var

--- vars/buildMaven.groovy
+++ vars/buildMaven.groovy
@@ -1,8 +1,8 @@
 def call(Map config = [:]) {
-    sh "echo ${config.release}"
-    sh "echo ${config.service_name}"
-    //sh "mvn clean install docker:build -Ddocker.image.name=esputnik/${config.service_name} -Ddocker.image.tag=${config.release}"
-    //sh "docker tag esputnik/${config.service_name}:${config.release} esputnik/${config.service_name}:${config.release}-no-tested"
-    //sh "docker push esputnik/${config.service_name}:${config.release}-no-tested"
+    sh "echo RELEASE_VERSION ${config.release}"
+    sh "echo SERVICE NAME ${config.service_name}"
+    sh "mvn clean install docker:build -Ddocker.image.name=esputnik/${config.service_name} -Ddocker.image.tag=${config.release}"
+    sh "docker tag esputnik/${config.service_name}:${config.release} esputnik/${config.service_name}:${config.release}-no-tested"
+    sh "docker push esputnik/${config.service_name}:${config.release}-no-tested"
     
 }
