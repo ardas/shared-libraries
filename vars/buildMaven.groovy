783e1c094e049b86c80e42cbfac4f72a7886bd41
Your Name <vitaliy.khatrus@gmail.com>
2021-05-27 13:37:34 +0300

    uncomment command in buildMaven shared lib

--- vars/buildMaven.groovy
+++ vars/buildMaven.groovy
@@ -1,8 +1,7 @@
 def call(Map config = [:]) {
-    //sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
     sh "echo ${config.release}"
-    //sh "mvn clean install docker:build -Ddocker.image.name=esputnik/rest-api -Ddocker.image.tag=${config.release}"
-    //sh "docker tag esputnik/rest-api:${config.release} esputnik/rest-api:${config.release}-no-tested"
-    //sh "docker push esputnik/rest-api:${config.release}-no-tested"
+    sh "mvn clean install docker:build -Ddocker.image.name=esputnik/rest-api -Ddocker.image.tag=${config.release}"
+    sh "docker tag esputnik/rest-api:${config.release} esputnik/rest-api:${config.release}-no-tested"
+    sh "docker push esputnik/rest-api:${config.release}-no-tested"
     
 }
