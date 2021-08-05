05f8a572e576da3676442b71847f3e53d121a4e9
Your Name <vitaliy.khatrus@gmail.com>
2021-08-05 10:50:18 +0300

    add doPostBuild lib

---
+++ vars/doPostBuild.groovy
@@ -0,0 +1,35 @@
+def call (){
+   post {
+         always {
+             cleanWs()
+         }
+         failure {
+             build(
+               job: 'failure-build-notification',
+               parameters: [
+                 [
+                   $class: 'StringParameterValue',
+                   name: 'FAIL_JOB_NAME',
+                   value: "$JOB_NAME",
+                ],
+                 [
+                   $class: 'StringParameterValue',
+                   name: 'FAIL_BUILD_URL',
+                   value: "$BUILD_URL",
+                 ],
+                 [
+                   $class: 'StringParameterValue',
+                   name: 'FAIL_GIT_COMMIT',
+                   value: "$GIT_COMMIT",
+                 ],
+                 [
+                   $class: 'StringParameterValue',
+                   name: 'FAIL_GIT_URL',
+                   value: "${GitURL}",
+                 ],
+               ]
+             )
+         }
+
+ }
+}
