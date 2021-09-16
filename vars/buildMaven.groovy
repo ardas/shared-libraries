eff68f44fa66acb6c973c78edea352cf2b9f2802
Your Name <vitaliy.khatrus@gmail.com>
2021-09-16 15:50:39 +0300

    add modification in buildMaven.groovy

--- vars/buildMaven.groovy
+++ vars/buildMaven.groovy
@@ -1,8 +1,23 @@
 def call(Map config = [:]) {
     sh "echo RELEASE_VERSION ${config.release}"
     sh "echo SERVICE NAME ${config.service_name}"
-    sh "mvn clean install docker:build -Ddocker.image.name=esputnik/${config.service_name} -Ddocker.image.tag=${config.release}"
-    sh "docker tag esputnik/${config.service_name}:${config.release} esputnik/${config.service_name}:${config.release}-no-tested"
-    sh "docker push esputnik/${config.service_name}:${config.release}-no-tested"
+    //sh "mvn clean install docker:build -Ddocker.image.name=esputnik/${config.service_name} -Ddocker.image.tag=${config.release}"
+    sh """
+        #!/bin/bash
+
+        if [ ${config.skipTests} = "false" ]; then
+           SKIP="-Dmaven.test.skip=true"
+           VCS_REVISION=${config.release}-"skip-tests"
+           echo "Test skipped"
+        else
+           VCS_REVISION=${config.release}
+        fi
+
+        echo $VCS_REVISION
+
+        #mvn clean install -Ddocker.image.tag=$VCS_REVISION -DskipTests=$SKIP
+        #docker tag esputnik/${config.service_name}:${config.release} esputnik/${config.service_name}:${config.release}-no-tested
+        #docker push esputnik/${config.service_name}:${config.release}-no-tested
+      """
     
 }
