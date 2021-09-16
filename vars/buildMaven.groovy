a3a42474876f5e8ab81160e6d449355f534dc835
Your Name <vitaliy.khatrus@gmail.com>
2021-09-16 16:29:03 +0300

    fix an errors

--- vars/buildMaven.groovy
+++ vars/buildMaven.groovy
@@ -2,7 +2,7 @@ def call(Map config = [:]) {
     sh "echo RELEASE_VERSION ${config.release}"
     sh "echo SERVICE NAME ${config.service_name}"
     //sh "mvn clean install docker:build -Ddocker.image.name=esputnik/${config.service_name} -Ddocker.image.tag=${config.release}"
-   if (${config.skipTests} == "false"){
+   if ("${config.skipTests}" == "false"){
       sh """
          #mvn clean install -Ddocker.image.tag=${config.release}"-skip-tests -Dmaven.test.skip=true
          echo ${config.release}-skip-tests
